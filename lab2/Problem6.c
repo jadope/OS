cd8c55fcb085f414e74761dcbdbe245762aa7c6b
jadope <jeronimoal10@gmail.com>
2018-02-26 10:45:44 -0500

    problem 6, bug, but fixable

--- lab2/Problem6.c
+++ lab2/Problem6.c
@@ -4,28 +4,37 @@
 #include <math.h>
 #include <string.h>
 
-int digitCounter(int n);
+int digitCounter(int);
+void resultLanz(char cadena[]);
 
 int main () { 
 srand(time(NULL)); //Inicializo la semilla pseudoaleatoria.
 int lanzarmoneda, random, i, j, digit;
 double lanzamiento;
-char resultado[];
-printf("Ingrese el número de veces de lanzar las monedas (585, por ejemplo, hará que se lance 5 veces, luego 8 y luego otras 5 veces): ");
+char resultado[11];
+printf("Ingrese el número de veces de lanzar las monedas.\n(585, por ejemplo, hará que se lance 5 veces, luego 8 y luego otras 5 veces): ");
 scanf("%d",&lanzarmoneda);
 digit = digitCounter(lanzarmoneda);
+printf("Valor de resultado[10]: %c\n", resultado[10]);
 for (i=0; i<digit; i++){
     lanzamiento=fmod(lanzarmoneda, pow(10, i+1))/pow(10,i); //Me retorna el último dígito del número ingresado.  
     printf("digit %d  --  lanzarmoneda %d   --- ", digit, lanzarmoneda);
     printf("Lanzamiento: %f \n", lanzamiento);
     for (j=0; j<floor(lanzamiento);j++){//Tengo que aplicar la función floor, para pasarlo de double a int y evitar que sobrepase el ciclo.
         random = rand()%(2);
-        if (random==1)
-        resultado[j]+="C"; //Si sale 1, agregue C a la cadena de caracteres.
-        else
-        resultado[j]+="S"; //Si sale 0, agregue Sa la cadena de caracteres. 
+        if (random==1){
+            resultado[j]+='C'; //Si sale 1, agregue C a la cadena de caracteres.
+            printf("Caracter: %c, random: %d, J: %d\n", resultado[j], random, j);
+        }
+        else if (random==0){
+            resultado[j]+='S'; //Si sale 0, agregue Sa la cadena de caracteres.
+            printf("Caracter: %c, random: %d, J: %d\n", resultado[j], random, j);
+        }
     }
-    resultLanz(resultado[]);
+    resultLanz(resultado);
+    for (j=0; j<lanzamiento;j++){ //Elimino los valores del vector anterior.
+        resultado[j]='\0';
+    }
 }
 return 0;   
 }
@@ -39,16 +48,16 @@ int digitCounter(int n){ //Esta función cuenta cuántos dígitos tiene el núme
     return count;
 }
 
-char resultLanz(char cadena[]){
-    int i = 0;
+void resultLanz(char cadena[]){
+    int i;
     int cara = 0;
     int sello = 0;
-    int longitud = strlen(cadena)
-    for(i; i<longitud; i++){
-        if(cadena[i]=="C")
-        cara+=1;
-        else
+    int longitud = strlen(cadena);
+    for(i = 0; i<longitud; i++){
+        if(cadena[i]=='C')
+            cara+=1;
+        else if (cadena[i]=='S')
         sello+=1;
     }
-    printf("%s, #de caras = %d, # de sellos = %d", cadena[], cara, sello);
+    printf("%s, #de caras = %d, # de sellos = %d\n", cadena, cara, sello);
 }
