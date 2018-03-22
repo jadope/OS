4937dff1f3ff75449a45f05507c4efca7259c618
davidblj <david.blj95@gmail.com>
2018-03-22 09:48:11 -0500

    agregar lectura de archivos

---
+++ practica 1/main.c
@@ -0,0 +1,62 @@
+
+/* 
+    nombre, - Name
+    estado, - State
+    text, - VmExe
+    data, - VmData
+    stack, - VmStk
+    cambios de contextos voluntarios - voluntary_ctxt-switches
+    cambio de contexto no voluntarios - nonvoluntary_ctxt-switches
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Data {
+    char name[100];
+};
+
+struct Data data;
+
+void getValue(char *identifier, char *string) {
+
+}
+
+void getData(char *buffer) {
+
+    // printf("%s", buffer);
+    char *line, *value;
+    char name[] = "Name:", divider = ':';
+
+    line = strstr(buffer, name);
+    
+    if (line != NULL) {
+        value = strchr(line, divider);
+        value++;
+        printf("%s", value);
+    }
+}
+
+void readFile() {
+
+    char filename[80] = "status";
+    char buffer[100];
+    FILE *file;
+
+    file = fopen(filename,"r");
+    
+    if(file == NULL) {
+        printf("file opening failed %s\n", filename);
+        exit(1);
+    }
+
+    while (fgets(buffer, 100, file) != NULL) {            
+        getData(buffer);
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    readFile();
+}
+
