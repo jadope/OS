946f587d876fd919b1e68c0c423a42c90b6bceef
davidblj <david.blj95@gmail.com>
2018-04-21 10:58:15 -0500

    agregar 5.5

---
+++ lab3-para-entregar/ejemplo5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main () {
+  int pid;
+  int numHijos = 5;
+  int numProceso;
+  int estado;
+
+  for(numProceso = 0; numProceso < numHijos; numProceso++) {
+
+      wait(&estado);  
+      pid = fork();
+
+      if (pid == -1) {
+          // Imprimir algún mensaje de error
+      } else if(pid == 0) {
+          break;
+      }
+  }
+
+  wait(&estado);  
+  
+  if (pid == 0) {
+      // Lógica del hijo
+      printf("Soy el hijo #%d\n", numProceso);
+  }
+  else {
+      printf("Soy un padre perezoso\n");
+  }
+}
