dce90d9d51838cb0e8d62b2822a93ed4d76ee948
davidblj <david.blj95@gmail.com>
2018-04-21 16:27:31 -0500

    agregar 10. Validaciones y system("pstree";

--- lab3-para-entregar/punto10.c
+++ lab3-para-entregar/punto10.c
@@ -12,43 +12,74 @@ int main(){
     int estado;
 
     ph1 = fork();
+  
+    if (ph1==-1){
+        perror("fork falló");
+        exit(1);
+    }
 
     if (ph1==0){ //hijo 1
 
         printf("Soy el hijo 1.\n");
+        printf("Hijo 1: Mi PID es %d\n", getpid());
+        printf("Hijo 1: El PID de mi padre es %d\n", getppid());
 
     }
     else//padre.
     {        
-        
         printf("Soy el padre.\n");
-        
+        printf("Padre: Mi PID es %d\n", getpid());
+        printf("Padre: El PID de mi padre es %d\n", getppid());
+        wait(&estado);
+
         ph2=fork();
+
+        if (ph2==-1){
+
+        perror("fork falló");
+        exit(1);
+        }
 
         if(ph2==0){//hijo 2
 
             printf("Soy el hijo 2.\n");
+            printf("Hijo 2: Mi PID es %d\n", getpid());
+            printf("Hijo 2: El PID de mi padre es %d\n", getppid());
+
             pn1=fork(); // creo el nieto 1
             
+            if (pn1==-1){
+                perror("fork falló");
+                exit(1);
+            }
+
             if (pn1==0){ // valido si es el nieto 1.
                 fflush(stdout);
                 printf("Soy el nieto 1.\n");
+                printf("Nieto 1: Mi PID es %d\n", getpid());
+                printf("Nieto 1: El PID de mi padre es %d\n", getppid());
+
 
             }
             else{ // sigue siendo el hijo 2.
                 wait(&estado);
                 pn2=fork();
+
+                if (pn2==-1){
+                    
+                    perror("fork falló");
+                    exit(1);
+                }
+
                 if(pn2==0){
+
                     fflush(stdout);
                     printf("Soy el nieto 2.\n");
-
+                    printf("Nieto 2: Mi PID es %d\n", getpid());
+                    printf("Nieto 2: El PID de mi padre es %d\n", getppid());
                 }
-            }
-            
-        }
-        // else {
-        //     printf("Soy el padre.\n");
-        // }
+            }            
+        }        
     }
-
+    system("pstree");
 }
