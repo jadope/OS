0268a9c9ca7dc465081fcd59d8bad81d863ba3d0
davidblj <david.blj95@gmail.com>
2018-05-05 15:10:30 -0500

    implementar 'separaItems'

--- lab3-p2-para-entregar/main.c
+++ lab3-p2-para-entregar/main.c
@@ -2,36 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-// *** ~ a pointer of an array of strings
-void getParams(char* commandLine, char*** params, int* backgroundFlag) {
-
-    *params = NULL;
-    char* token = strtok(commandLine, " ");
-    printf("mem address: %p", *params);
-    int tokenCounter = 0;
-
-    // while(token) {
-    //     *params = realloc(commandLine, sizeof (char*) * ++tokenCounter);                
-
-    //     if (params == NULL)
-    //         exit(-1);
-
-    //     // params[tokenCounter-1] = token;
-    //     token = strtok (NULL, " ");
-    // }
-}
+#include "parser.h"
+#define TAM 100
 
 int main(int argc, char const *argv[]) {
 
-    int backgroundFlag = 0;
-    char commandLine[200];
-
-    // array of strings
-    char** params;   
-    printf("initial mem address: %p", *params);
+    int background, num;    
+    char expresion[TAM];
+    char** items;   
 
     printf("\nSO@cli $ ");
-    scanf("%s", commandLine);
+    fgets (expresion, TAM, stdin);
 
-    getParams(commandLine, &params, &backgroundFlag);
+    num = separaItems (expresion, &items, &background);    
+
+    printf ("Numero de parametros: %d\n", num);
 }
